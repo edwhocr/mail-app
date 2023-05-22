3e0456a9dd307bb8ea748160eaeba87bf62f5857
edwhocr <edwinchasing209692@gmail.com>
2023-05-21 19:50:03 -0500

    test5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import {Inicio} from './Pages/Home/Home';
 import {Usuario1} from './Pages/Users/Usuario1';
 import {Usuario2} from './Pages/Users/Usuario2';
-import { NotFoundPage } from './Pages/Errors/404';
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
       <Route path="/mail-app" element={<Inicio/>} />
       <Route path="/mail-app/usuario/1" element={<Usuario1/>} />
       <Route path="/mail-app/usuario/2" element={<Usuario2/>} />
-      <Route path="/" element={<Inicio/>} />
+      <Route path="*" element={<Inicio/>} />
      </Routes>
 
   );
